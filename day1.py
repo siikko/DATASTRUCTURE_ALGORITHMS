de61dbd28dfe5c2a767d244080040b2a0a7fb9ce
DESKTOP-DP2C5J4\USER <siko2828@naver.com>
2025-02-17 10:41:13 +0900

    v0.4 conversion

--- day1.py
+++ day1.py
@@ -3,3 +3,30 @@ b=10
 print(a&b)
 print(a|b)
 print(a^b)
+
+def is_even(n) -> bool:
+    """
+    짝수 판정 함수
+    :param n: 판정할 정수
+    :return:  짝수면 True, 홀수면 False
+    """
+
+    if n%2==0:
+        return True
+    return False
+
+#비트연산자 사용해서 함수구현
+def is_even_bit(n)->bool:
+    return not n&1
+
+#n=int(input())
+
+#입력값에 대한 8진수 함수구현
+def dec_oct(n) -> int:
+    if n==0:
+        return ""
+    else:
+        return dec_oct(n//8) + str(n%8)
+n = int(input())
+print(dec_oct(n))
+
