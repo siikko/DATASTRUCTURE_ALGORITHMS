7c4f9bbc7151e7e11bccf93349da6a2ef6501a6f
DESKTOP-DP2C5J4\USER <siko2828@naver.com>
2025-02-18 10:02:08 +0900

    v0.1 ãstack수정

--- day2.py
+++ day2.py
@@ -2,18 +2,20 @@ class Stack:
     def __init__(self):
         self.items=list() #스택만들면 제일 먼저하는 것: 리스트 만들기
 
-    def push(self,data):
+    def push(self,data)->object:
         self.items.append(data)
 
-    def pop(self):
+    def pop(self)->object:
         return self.items.pop() #리턴 후, 삭제
 
-    def size(self):
+    def size(self)->int:
         return len(self.items)
 
-    def peek(self):
+    def peek(self)->object:
         return self.items[-1] #확인용도
 
+    def is_empty(self)->bool:
+        return len(self.items)==0
 
 s1=Stack()
 s1.push(-9)
@@ -22,3 +24,6 @@ s1.push(977)
 print(s1.pop())
 print(s1.peek())
 print(s1.peek())
+print(s1.pop())
+print(s1.pop())
+print(s1.is_empty())
