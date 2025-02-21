a91abf2ccf8f6c1cc7f677e9f715ed07a7d1dc99
DESKTOP-DP2C5J4\USER <siko2828@naver.com>
2025-02-21 10:23:27 +0900

    v0.4 sort_update

--- day05.py
+++ day05.py
@@ -1,9 +1,10 @@
 def bubble_sort(lst):
     lst_length=len(lst)-1
     for i in range(lst_length):
-        for j in range(lst_length):
+        for j in range(lst_length-i):
             if lst[j]>lst[j+1]:
                 lst[j],lst[j+1]=lst[j+1],lst[j]
+            print(j,end=' ')
     return lst
 lst=[2,7,1,9]
 print(bubble_sort(lst))
