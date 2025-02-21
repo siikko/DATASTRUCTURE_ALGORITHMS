1f19ac2e7cc0080315bfa30ac6dca53745cf26b1
DESKTOP-DP2C5J4\USER <siko2828@naver.com>
2025-02-21 10:27:50 +0900

    v0.4 sort update flag

--- day05.py
+++ day05.py
@@ -1,10 +1,16 @@
 def bubble_sort(lst):
     lst_length=len(lst)-1
     for i in range(lst_length):
+        no_swap=True
         for j in range(lst_length-i):
             if lst[j]>lst[j+1]:
                 lst[j],lst[j+1]=lst[j+1],lst[j]
-            print(j,end=' ')
+                no_swap = False
+                print(j,end=' ')
+        if no_swap:
+            return lst
     return lst
 lst=[2,7,1,9]
 print(bubble_sort(lst))
+lst_=[1,2,3,4]
+print(bubble_sort(lst_))
